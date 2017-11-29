6a07cd604152d23201f1038739d82a4e2df9daba
camivar1397 <camivar1397@hotmail.com>
2017-11-28 22:05:33 -0500

    heroku full, y todas las tablas

--- models/Transaccion.js
+++ models/Transaccion.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Transaccion = new Schema({
+var transaccion = new Schema({
 	idEstadoTransaccionT: { type: Schema.Types.ObjectId, ref: "EstadoTransaccion" },
 	idCarroComprasT: { type: Schema.Types.ObjectId, ref: "CarroCompras" },
 	fechaTransaccion: { type: Date, default: Date.now },
 	tipoTransaccion: String
 });
 
-module.exports=mongoose.model('CarroCompras',carroCompras);
+module.exports=mongoose.model('Transaccion',transaccion);
